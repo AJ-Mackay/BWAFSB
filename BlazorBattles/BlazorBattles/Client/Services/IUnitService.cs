5d86031dd770a638ef654944119c4b143cd10584
Paul Mackay <paul.m.mackay85@gmail.com>
2021-01-12 22:25:14 +0000

    Build Units with the UnitService

---
+++ BlazorBattles/BlazorBattles/Client/Services/IUnitService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Client.Services
+{
+    public interface IUnitService
+    {
+        IList<Unit> Units { get; }
+        IList<UserUnit> MyUnits { get; set; }
+        void AddUnit(int unitId);
+    }
+}
